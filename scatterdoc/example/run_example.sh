18d32b9bdc2b03bd761bb155e6e0c3b426290cde
Stephen Masters <stephen.masters@me.com>
2019-10-16 15:41:21 +0100

    Removing some tests that werent meant to be here

--- scatterdoc/example/run_example.sh
+++ scatterdoc/example/run_example.sh
@@ -2,11 +2,5 @@
 
 docker run --rm -i  \
     -v "`pwd`/in:/home/pandoc/workspace/in" -v "`pwd`/out:/home/pandoc/workspace/out" \
-    scattercode/pandoc:1.19.2.4 \
+    scattercode/scatterdoc:1.0 \
     pandoc in/test.html -f html -t latex -o out/test.pdf
-
-
-docker run --rm -i  \
-    -v "`pwd`/in:/home/pandoc/workspace/in" -v "`pwd`/out:/home/pandoc/workspace/out" \
-    scattercode/pandoc:1.0 \
-    pandoc -v
